56080d352035a391919b498f33edd3dcd39dc4e6
DK\dok <dok@sitecore.net>
2017-11-17 13:28:15 +0200

    two les

--- Program.cs
+++ Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace MainNameSpoace
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
+            MainAsinc(args).GetAwaiter().GetResult();
+            Console.WriteLine();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsinc(string[] args)
+        {
+            var client = new MongoClient();
         }
     }
 }
---
+++ mClicntmDBMcol.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace ConsoleApp1
+{
+    class mClicntmDBMcol
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+
+            var db = client.GetDatabase("test");
+            //var collection = db.GetCollection<Person>("people");
+        }
+    }
+}
